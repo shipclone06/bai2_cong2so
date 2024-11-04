acccff4d4d2633b6cfc063feb78f7f284be49507
shipclone06 <anhkhoa030900@gmail.com>
2024-11-04 14:18:20 +0700

    sử dụng cin để nhập

--- bai2_cong2so/bai2_cong2so.cpp
+++ bai2_cong2so/bai2_cong2so.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main() {
 	cout << " CHUONG TRINH TINH TOAN TRONG HAI SO" << endl;
-	int so1, so2; //khai bao bien so1 va so2 kieu so nguyen;
+	int so1, so2; //khai bao bien so1 va so2 kieu so nguyen
 	int tong;
-	so1 = 3; // gan bien so1 voi gia tri 3
-	so2 = 9;
+	cout << "nhap so1:"; cin >> so1;
+	cout << "nhap so2:"; cin >> so2;
 	tong = so1 + so2;
-	cout << "tong hai so:" << so1 << "+" << so2 << "=" << tong;
-	return 0;
+	cout << "tong hai so:" << so1 << "+" << so2 << "=" << tong << endl;
+	cout << "tong hai so:" << so1 << "+" << so2 << "=" << so1 + so2;
+
+ 	  return 0;
 }
